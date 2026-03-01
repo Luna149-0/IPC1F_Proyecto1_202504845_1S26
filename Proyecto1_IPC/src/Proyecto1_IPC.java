809a6cdf3b536af6d6f6e91e6ed5337f9e224b7f
Ixchel Panjoj <ixchelpanjoj@gmail.com>
2026-03-01 15:12:10 -0600

    Menu del proyecto 1

---
+++ Proyecto1_IPC/src/Proyecto1_IPC.java
@@ -0,0 +1,15 @@
+
+import proyecto1_ipc.views.VistaPrincipal;
+
+
+public class Proyecto1_IPC {
+
+    public static void main(String[] args) {
+        
+        VistaPrincipal vista= new VistaPrincipal();
+        vista.setVisible(true);
+        vista.setLocationRelativeTo(null);
+
+    }
+    
+}
